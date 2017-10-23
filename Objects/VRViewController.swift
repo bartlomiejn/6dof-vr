9b1cca0c5d9046081cd8c7432a8c601af30ecc38
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2017-10-23 21:24:05 +0200

    Added ARSession, random generation of geometry and adjusted camera settings

--- Objects/VRViewController.swift
+++ Objects/VRViewController.swift
@@ -14,15 +14,17 @@ final class VRViewController: UIViewController {
     
     private enum Constant {
         enum Distance {
-            static let interpupilary = 0.066 // Distance between eyes in meters
+            static let interpupilary: Double = 0.066 // Distance between eyes in meters
         }
     }
     
     @IBOutlet private weak var leftSceneView: SCNView!
     @IBOutlet private weak var rightSceneView: SCNView!
     
-    private let motionManager = CMMotionManager()
+    private let session = ARSession()
     private let configuration = ARWorldTrackingConfiguration()
+    
+    private let motionManager = CMMotionManager()
     
     private let scene = SCNScene()
     
@@ -31,6 +33,9 @@ final class VRViewController: UIViewController {
     private let leftCameraNode = SCNNode()
     private let rightCameraNode = SCNNode()
     
+    private var viewportPosition = SCNVector3(0.0, 0.0, 0.0)
+    private var lastPanTranslation: CGPoint?
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
@@ -38,11 +43,20 @@ final class VRViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupCamera(leftCamera)
+        setupCamera(rightCamera)
+        
         setup(leftSceneView)
         setup(rightSceneView)
-        setupSceneGeometry()
-        setupPointOfView()
-        setupMotionUpdates()
+        
+        addBoxNodes(count: 40)
+        setupPlaneNode()
+        setupPointOfViewNodes()
+        
+        setupPanGestureRecognizer()
+        
+        startARSession()
+        startMotionUpdates()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -56,44 +70,57 @@ final class VRViewController: UIViewController {
         
         setSceneViewPlaying(to: false)
     }
+    
+    private func setupCamera(_ camera: SCNCamera) {
+        camera.fieldOfView = 90.0
+        camera.zNear = 0.1
+        camera.zFar = 100.0
+    }
 
     private func setup(_ sceneView: SCNView) {
         sceneView.scene = scene
-        sceneView.preferredFramesPerSecond = 120
+        sceneView.preferredFramesPerSecond = 60
         sceneView.autoenablesDefaultLighting = true
+        sceneView.antialiasingMode = .multisampling4X
+        sceneView.isUserInteractionEnabled = true
     }
-    
-    private func setupSceneGeometry() {
-        setupBoxNode()
-        setupPlaneNode()
+
+    private func addBoxNodes(count: Int) {
+        for _ in 0..<count {
+            let box = SCNNode(geometry:
+                SCNBox(
+                    width: random(lowerLimit: 0.2, upperLimit: 1.0),
+                    height: random(lowerLimit: 0.5, upperLimit: 6.0),
+                    length: random(lowerLimit: 0.2, upperLimit: 1.0),
+                    chamferRadius: 0.0))
+            
+            box.position = SCNVector3(
+                random(lowerLimit: -10.0, upperLimit: 10.0),
+                0.0,
+                random(lowerLimit: -10.0, upperLimit: -1.0))
+            box.geometry?.firstMaterial?.diffuse.contents = UIColor.white
+            
+            scene.rootNode.addChildNode(box)
+        }
     }
-    
-    private func setupBoxNode() {
-        let box = SCNNode(geometry: SCNBox(width: 0.5, height: 0.5, length: 0.5, chamferRadius: 0.0))
-        
-        box.position = SCNVector3(0.2, 0.0, -4.0)
-        box.geometry?.firstMaterial?.diffuse.contents = UIColor.red
-        
-        scene.rootNode.addChildNode(box)
-    }
-    
+
     private func setupPlaneNode() {
         let plane = SCNNode(geometry: SCNPlane(width: 50.0, height: 50.0))
         
         plane.position = SCNVector3(0.0, -0.5, 0.0)
         plane.eulerAngles = SCNVector3(90.degreesToRadians, 0.0, 0.0)
-        plane.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        plane.geometry?.firstMaterial?.diffuse.contents = UIColor.black
         plane.geometry?.firstMaterial?.isDoubleSided = true
         
         scene.rootNode.addChildNode(plane)
     }
     
-    private func setupPointOfView() {
+    private func setupPointOfViewNodes() {
         leftCameraNode.camera = leftCamera
         rightCameraNode.camera = rightCamera
         
-        leftCameraNode.position = SCNVector3(-Constant.Distance.interpupilary / 2.0, 1.75, 0.0)
-        rightCameraNode.position = SCNVector3(Constant.Distance.interpupilary / 2.0, 1.75, 0.0)
+        leftCameraNode.position = SCNVector3(-Constant.Distance.interpupilary /*/ 2.0*/, 1.75, 0.0)
+        rightCameraNode.position = SCNVector3(Constant.Distance.interpupilary /*/ 2.0*/, 1.75, 0.0)
         
         scene.rootNode.addChildNode(leftCameraNode)
         scene.rootNode.addChildNode(rightCameraNode)
@@ -102,7 +129,37 @@ final class VRViewController: UIViewController {
         rightSceneView.pointOfView = rightCameraNode
     }
     
-    private func setupMotionUpdates() {
+    private func setupPanGestureRecognizer() {
+        let recognizer = UIPanGestureRecognizer(target: self, action: #selector(pannedView))
+        view.addGestureRecognizer(recognizer)
+    }
+    
+    @objc private func pannedView(recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: view)
+        
+        switch recognizer.state {
+            case .began:
+                lastPanTranslation = translation
+            case .changed:
+                guard let lastPanTranslation = lastPanTranslation else {
+                    return
+                }
+                
+                viewportPosition = viewportPosition
+                    + SCNVector3(lastPanTranslation.x - translation.x, 0.0, lastPanTranslation.y - translation.y)
+                
+                self.lastPanTranslation = translation
+            default:
+                lastPanTranslation = nil
+        }
+    }
+    
+    private func startARSession() {
+        session.delegate = self
+        session.run(configuration)
+    }
+    
+    private func startMotionUpdates() {
         motionManager.deviceMotionUpdateInterval = 1.0 / 120.0
         
         guard motionManager.isDeviceMotionAvailable else {
@@ -131,40 +188,25 @@ final class VRViewController: UIViewController {
     }
 }
 
-extension VRViewController: SCNSceneRendererDelegate {
-    
-}
-
-extension VRViewController: ARSCNViewDelegate {
-    
-    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
-        
-    }
-    
-    func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
-
-    }
-    
-    private func currentCameraPosition() -> SCNVector3? {
-        guard let pov = leftSceneView.pointOfView else {
-            return nil
-        }
-        
-        let transform = pov.transform
-        let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
-        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
-        
-        return orientation + location
-    }
-}
-
 extension VRViewController: ARSessionDelegate {
     
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
-
+        let translationColumn = frame.camera.transform.columns.3
+        
+        leftCameraNode.position =
+            SCNVector3(
+                translationColumn.x - Float(Constant.Distance.interpupilary),
+                translationColumn.y,
+                translationColumn.z)
+            + viewportPosition
+        rightCameraNode.position =
+            SCNVector3(
+                translationColumn.x + Float(Constant.Distance.interpupilary),
+                translationColumn.y,
+                translationColumn.z)
+            + viewportPosition
     }
 }
-
 
 func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
     return SCNVector3(left.x + right.x, left.y + right.y, left.z + right.z)
@@ -172,4 +214,8 @@ func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
 
 extension Int {
     var degreesToRadians: Double { return Double(self) * .pi / 180 }
+}
+
+func random(lowerLimit: CGFloat, upperLimit: CGFloat) -> CGFloat {
+    return lowerLimit + (upperLimit - lowerLimit) * (CGFloat(arc4random()) / CGFloat(UInt32.max))
 }
